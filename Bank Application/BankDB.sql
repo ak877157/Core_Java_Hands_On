4338edb17fe0fdd435258973c179f046f95e8b1c
Amit <ak877157@gmail.com>
2022-02-23 09:08:53 +0530

    Adding Bank Application

---
+++ Bank Application/BankDB.sql
@@ -0,0 +1,15 @@
+USE bankdb;
+
+
+INSERT INTO `bankdb`.`bank` 
+	(`Acc_No`, 
+	`Customer_Name`, 
+	`AVL_Balance`, 
+	`Ph_No`
+	)
+	VALUES
+	('245900', 'Amit', '100000', '835288'),
+	('245901', 'Kavita', '60000', '986476'),
+	('245902', 'Ankush', '25000', '3456789'),
+	('245903', 'Vikash', '10000', '2345678'),
+	('245904', 'Rashmika', '8000', '123456');
